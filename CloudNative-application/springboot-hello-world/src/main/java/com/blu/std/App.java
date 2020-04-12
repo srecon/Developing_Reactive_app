fb316db7a007fbb74707ea8d12ba2789cbb4bc51
shamim <shamimapache@gmail.com>
2020-04-12 22:26:02 +0300

    add MiniShift configurations

--- CloudNative-application/springboot-hello-world/src/main/java/com/blu/std/App.java
+++ CloudNative-application/springboot-hello-world/src/main/java/com/blu/std/App.java
@@ -21,18 +21,5 @@ public class App
         LOGGER.info("Spring Boot Hello-World app runs!");
         SpringApplication.run(App.class);
     }
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
 }
