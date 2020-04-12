c5737a336099e2a2a0acd91f50545b5bc1b57ed4
shamim <shamimapache@gmail.com>
2020-04-12 17:39:09 +0300

    add MiniShift configurations

--- CloudNative-application/springboot-hello-world/src/main/java/com/blu/std/App.java
+++ CloudNative-application/springboot-hello-world/src/main/java/com/blu/std/App.java
@@ -1,5 +1,7 @@
 package com.blu.std;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +14,11 @@ import java.util.Arrays;
 @SpringBootApplication
 public class App 
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Spring Boot app runs!" );
+        LOGGER.info("Spring Boot Hello-World app runs!");
         SpringApplication.run(App.class);
     }
     @Bean
--- CloudNative-application/springboot-hello-world/src/main/java/com/blu/std/DemoController.java
+++ CloudNative-application/springboot-hello-world/src/main/java/com/blu/std/DemoController.java
@@ -1,5 +1,7 @@
 package com.blu.std;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,14 +9,24 @@ import java.util.Random;
 
 @RestController
 public class DemoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
+
     @RequestMapping("/")
     public String index() {
-        return "Spring Boot application for demo!";
+        return "Spring Boot Hello-world application for demo!";
     }
+
     @RequestMapping("/getQuote")
     public String getQuote() {
-        final String[] todaysQuote = new String[]{"Today you are you!","Today was good.", "Today is the only day.", "What is not started today is never finished tomorrow."};
+        final String[] todaysQuote = new String[]{
+                "Today you are you!",
+                "Today was good.",
+                "Today is the only day.",
+                "What is not started today is never finished tomorrow."
+        };
         final int rnd = new Random().nextInt(todaysQuote.length);
+
         return todaysQuote[rnd];
     }
 
