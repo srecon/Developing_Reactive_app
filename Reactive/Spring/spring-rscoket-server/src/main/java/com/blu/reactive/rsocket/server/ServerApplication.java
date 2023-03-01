9ce5b6116f9778e97cbe591b6f482440675dbc7d
Shamim <srecon@yandex.ru>
2023-03-01 20:01:14 +0300

    Add spring rsocket Client example

---
+++ Reactive/Spring/spring-rscoket-server/src/main/java/com/blu/reactive/rsocket/server/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.blu.reactive.rsocket.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .main(ServerApplication.class)
+                .sources(ServerApplication.class)
+                .profiles("server")
+                .run(args);
+    }
+}
