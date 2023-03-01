46fac446ccc56895b9339b9ec54057806bf1cf6f
Shamim <srecon@yandex.ru>
2023-03-01 14:23:31 +0300

    Add spring rsocket example

---
+++ Reactive/Spring/spring-rscoket/src/test/java/com/blu/reactive/springrscoket/SpringRscoketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blu.reactive.springrscoket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRscoketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
