2c446410d709f355a192f07799c6b2c9754b7750
Shamim <srecon@yandex.ru>
2023-03-02 16:55:22 +0300

    Add spring rsocket fire&forget/streming/server calling examples

--- Reactive/Spring/spring-rscoket-client/src/main/java/com/blu/reactive/rsocket/client/RSocketShellClient.java
+++ Reactive/Spring/spring-rscoket-client/src/main/java/com/blu/reactive/rsocket/client/RSocketShellClient.java
@@ -1,14 +1,21 @@
 package com.blu.reactive.rsocket.client;
 
 import com.blu.reactive.rsocket.data.Message;
+import io.rsocket.SocketAcceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.util.MimeTypeUtils;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Slf4j
@@ -18,25 +25,44 @@ public class RSocketShellClient {
 
     private static final String CLIENT = "Client";
     private static final String REQUEST = "Request";
+    private static final String FIRE_AND_FORGET = "Fire&Forget";
+    private static final String STREAM = "Streaming";
 
-    @Bean
-    public RSocketRequester getRSocketRequester() {
+    private static Disposable disposable;
 
+//    @Autowired
+//    public RSocketShellClient(RSocketRequester.Builder rsocketRequesterBuilder) {
+//        String client = UUID.randomUUID().toString();
+//        log.info("Connecting using Client ID {}", client);
+//        this.rsocketRequester = rsocketRequesterBuilder.connectTcp("localhost",7007).block();
+//    }
+    @Autowired
+    public RSocketShellClient(RSocketRequester.Builder rsocketRequesterBuilder, RSocketStrategies strategies) {
+
+        // (1)
         String client = UUID.randomUUID().toString();
         log.info("Connecting using client ID: {}", client);
 
-        RSocketRequester.Builder builder = RSocketRequester.builder();
+        // (2)
+        SocketAcceptor responder = RSocketMessageHandler.responder(strategies, new ClientHandler());
 
-        return this.rsocketRequester = builder
-                                            .dataMimeType(MimeTypeUtils.APPLICATION_JSON)
-                                            .connectTcp("localhost",7007).block();
+        // (3)
+        this.rsocketRequester = rsocketRequesterBuilder
+                .setupRoute("shell-client")
+                .setupData(client)
+                .rsocketStrategies(strategies)
+                .rsocketConnector(connector -> connector.acceptor(responder))
+                .connectTcp("localhost", 7007)
+                .block();
+
+        // (4)
+        this.rsocketRequester.rsocket()
+                .onClose()
+                .doOnError(error -> log.warn("Connection CLOSED"))
+                .doFinally(consumer -> log.info("Client DISCONNECTED"))
+                .subscribe();
     }
-//    @Autowired
-//    public RSocketShellClient(RSocketRequester.Builder rsocketRequesterBuilder) {
-//
-//
-//        this.rsocketRequester = rsocketRequesterBuilder.connectTcp("localhost",7007).block();
-//    }
+    // patterns goes here
     @ShellMethod("Send one request. One response will be printed.")
     public void requestResponse() throws InterruptedException {
         log.info("\nSending one request. Waiting for one response...");
@@ -46,5 +72,39 @@ public class RSocketShellClient {
                 .retrieveMono(Message.class)
                 .block();
         log.info("\nResponse was: {}", message);
+    }
+    @ShellMethod("Send one request. No response will be returned.")
+    public void fireAndForget() throws InterruptedException {
+        log.info("\nFire-And-Forget. Sending one request. Expect no response (check server log)...");
+        this.rsocketRequester
+                .route("fire-and-forget")
+                .data(new Message(CLIENT, FIRE_AND_FORGET))
+                .send()
+                .block();
+    }
+    @ShellMethod("Send one request. Many responses (stream) will be printed.")
+    public void stream() {
+        log.info("\nRequest-Stream. Sending one request. Waiting for unlimited responses (Stop process to quit)...");
+        this.disposable = this.rsocketRequester
+                .route("stream")
+                .data(new Message(CLIENT, STREAM))
+                .retrieveFlux(Message.class)
+                .subscribe(er -> log.info("Response received: {}", er));
+    }
+    @ShellMethod("Stop streaming messages from the server.")
+    public void s(){
+        if(null != disposable){
+            disposable.dispose();
+        }
+    }
+    // Inner class for handling server requests
+    @Slf4j
+    static class ClientHandler {
+
+        @MessageMapping("client-status")
+        public Flux<String> statusUpdate(String status) {
+            log.info("Connection {}", status);
+            return Flux.interval(Duration.ofSeconds(5)).map(index -> String.valueOf(Runtime.getRuntime().freeMemory()));
+        }
     }
 }
--- Reactive/Spring/spring-rscoket-server/src/main/java/com/blu/reactive/rsocket/server/RSocketController.java
+++ Reactive/Spring/spring-rscoket-server/src/main/java/com/blu/reactive/rsocket/server/RSocketController.java
@@ -3,17 +3,64 @@ package com.blu.reactive.rsocket.server;
 import com.blu.reactive.rsocket.server.data.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
 public class RSocketController {
     private static final String SERVER = "Server";
     private static final String RESPONSE = "Response";
+    private static final String STREAM = "streaming";
+
+    private final List<RSocketRequester> CLIENTS = new ArrayList<>();
 
     @MessageMapping("request-response")
     Message requestResponse(Message request) {
        log.info("Received request-response request: {}", request);
        return new Message(SERVER, RESPONSE);
+    }
+    @MessageMapping("fire-and-forget")
+    public void fireAndForget(Message request){
+        log.info("Received fire&forget request {}", request);
+    }
+    @MessageMapping("stream")
+    Flux<Message> stream(Message request) {
+        log.info("Received stream request: {}", request);
+        return Flux
+                .interval(Duration.ofSeconds(1))
+                .map(index -> new Message(SERVER, STREAM, index))
+                .log();
+    }
+
+    @ConnectMapping("shell-client")
+    void connectShellClientAndAskForTelemetry (RSocketRequester requester, @Payload String client) {
+        requester.rsocket()
+                .onClose() // (1)
+                .doFirst(() -> {
+                    log.info("Client: {} CONNECTED.", client);
+                    CLIENTS.add(requester); // (2)
+                })
+                .doOnError(error -> {
+                    log.warn("Channel to client {} CLOSED", client); // (3)
+                })
+                .doFinally(consumer -> {
+                    CLIENTS.remove(requester);
+                    log.info("Client {} DISCONNECTED", client); // (4)
+                })
+                .subscribe();
+
+        requester.route("client-status")
+                .data("OPEN")
+                .retrieveFlux(String.class)
+                .doOnNext(s -> log.info("Client: {} Free Memory: {}.",client,s))
+                .subscribe();
     }
 }
