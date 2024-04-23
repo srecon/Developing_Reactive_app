85e114a37bd405f7039ee91a69dd263382000ee2
shamim <srecon@yandex.ru>
2024-04-23 15:20:02 +0300

    spring-grpc base project

---
+++ 21issues/grpc/grpc-server/src/test/java/com/blu/grpc/grpcserver/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blu.grpc.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
